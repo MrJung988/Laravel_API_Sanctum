810cef79d1688ad20f7cf0ebef36611fc3abe15c
MrJung988 <karkisamir99@gmail.com>
2023-06-02 08:55:18 +0545

    Login authentication done

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/', function (Request $request) {
     return $request->user();
 });
 
 
 // Public Routes
-Route::get('/students', [StudentController::class, 'index']);
-Route::get('/students/{id}', [StudentController::class, 'show']); 
+Route::post('/login', [UserController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/{id}', [StudentController::class, 'show']);
+});
